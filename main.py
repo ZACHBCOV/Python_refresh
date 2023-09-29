2581c38efcf254e0b891faddf293a1979ce35461
ZACHBCOV <118753820+ZACHBCOV@users.noreply.github.com>
2023-09-29 14:58:36 +0100

    Timothy needham book - 29.09.23

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 #python for begginers Timothy needham
+# imports and variables
+import math
+
+a = 2
+b = 2
+c=math.sqrt(a**2+b**2)
+print(c)
+
+# loops
+for name in "john","sam","jill":
+    print("hello" +" "+ name)
+
+for i in range(10):
+ print(i)
+
+
+total = 0
+for i in 5, 7, 11, 13:
+    print(i+total)
+    total=total+i
+print(total)
+# total added in print i line 3. Without = prints each number as it occurs then totals all. With total
+# ... it gives a running total upto the final value i.e. 36. i.e. 5 --> 5+7 ---> 5+7+11---> 5+7+11+13 END
+
+# to indent (i.e. add for spaces = TAB key
+    #ENTER CODE HERE
+# i in loops stands for 'index' - the index can be replced by any relevant name even _ where we do not
+#.. care about the value of 'i'
+
+
+
 
